3d7153ff279ed9e1f624dabbf19a89c9e983de6f
Derrick <derrickx101@gmail.com>
2025-05-27 16:28:33 -0700

    discord bot test

--- src/main/java/com/tetrisbot/Discord.java
+++ src/main/java/com/tetrisbot/Discord.java
@@ -1,53 +1,26 @@
 package com.tetrisbot;
 
-/**
- * This class handles interactions with the Discord API. Each instance should
- * be deployed to a separate channel.
- * Status codes:
-    * 0 = success
-    * 1 = ... (work in progress)
- */
+import javax.security.auth.login.LoginException;
 
-public class Discord {
-    //Add static variables here as necessary
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-    String guildId;
-    String channelId;
-    //Add instance variables here as necessary
-
-    /**
-     * Links the instance of the bot to the channel in the server specified by
-     * the parameters. Returns status code based on result of link attempt.
-     * @param guild The id of the server that contains the channel to link to.
-     * @param channel The id of the channel to link to.
-     * @return An integer representing the result of the link attempt, as
-     * mapped in the class description.
-     */
-    public int linkToChannel(String guild, String channel) {
-        return 0;
+public class Discord extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException {
+        String token = "";
+        JDABuilder.createDefault(token)
+                .addEventListeners(new Discord())
+                .build();
     }
 
-    /**
-     * Sends the specified message to the channel that the bot is linked to.
-     * @param text The message content to be sent.
-     * @return An integer representing the result of the messaging attempt, as
-     * mapped in the class description.
-     */
-    public int sendMessage(String text) {
-        return 0;
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) {
+            return;
+        }
+        if (event.getMessage().getContentRaw().equalsIgnoreCase("!hi")) {
+            event.getChannel().sendMessage("Hello, " + event.getAuthor().getName() + "!").queue();
+        }
     }
-
-    /**
-     * Sends the specified message to the specified user's direct messages.
-     * @param user The id of the user to send the message to.
-     * @param text The message content to be sent.
-     * @return An integer representing the result of the messaging attempt, as
-     * mapped in the class description.
-     */
-    public int sendDirectMsg(String user, String text) {
-        return 0;
-    }
-    //addReaction()
-
-    //retrieveReactions()
 }
